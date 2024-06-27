f29d75ceea2a98a935e5a0756c7d57dde920feef
OmkarGeedh <40747153+OmkarGeedh@users.noreply.github.com>
2024-06-27 18:26:05 +0530

    express

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const express = require('express')
+
+const app = express ()
+
+app.get('/auth/google', (req, res) => {
+  res.redirect(googleAuthUrl);
+});
+
+app.get('/auth/google/callback', async (req, res) => {
+  const { code } = req.query;
+  const { tokens } = await oauth2Client.getToken(code);
+  oauth2Client.setCredentials(tokens);
+
+  
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
